c3af489b4bdc6b4f21a813f0854c3e877bdedbd6
Gonçalo Mendes <goncalo.mendes@mindswap.academy>
2022-08-19 17:43:43 +0100

    server - client communication

--- src/academy/mindswap/game/Game.java
+++ src/academy/mindswap/game/Game.java
@@ -3,7 +3,7 @@ package academy.mindswap.game;
 import academy.mindswap.server.Server;
 
 import java.util.ArrayList;
-
+//gm3nd3s code
 public class Game implements Runnable{
     private ArrayList<Server.ClientHandler> listOfClients;
     @Override
