be0e01d9084c944144b8a9a183b0a548128eda56
joaoguima24 <joao.guimaraes@mindswap.academy>
2022-08-24 12:14:50 +0100

    Fixed the bug of playing a new game

--- src/academy/mindswap/server/Game.java
+++ src/academy/mindswap/server/Game.java
@@ -207,7 +207,7 @@ public class Game implements Runnable{
         if(matcherBefore.find()){
             position1 = Integer.parseInt(matcherBefore.group());
         }
-        if (position1 < 0 || position1+1 > timelineDeck.size()){
+        if (position1 < 0 || (position1+1) > timelineDeck.size()){
             invalidPlay();
         }
         validatePlay(indexCardByAsciiVal, position1);
@@ -258,8 +258,8 @@ public class Game implements Runnable{
             try {
                 if (!client.listenToClient().equalsIgnoreCase("yes")){
                     client.socket.close();
-                }
-                client.addMeToNewGame();
+                } else {client.addMeToNewGame();}
+
             } catch (IOException e) {
                 client.getPlayersOnline().remove(client);
             }
